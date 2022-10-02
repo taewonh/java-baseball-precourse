39d1846354f9092ac62837397dcc2318b51a833c
taewonh <40269387+taewonh@users.noreply.github.com>
2022-10-02 21:51:35 +0900

    feat: Computer와 User의 값을 비교하여 결과를 판정하는 Referee 클래스 추가
    - Referee 클래스 추가 기능
       - judgeInning : User로부터 값을 입력받을 때마다 외부에서 호출할 수 있는 판정 기능
       - initBallCount : 결과를 판정할 때마다 항상 볼카운트를 0으로 초기화하는 기능
       - judgeBallCount : Computer와 User의 값을 비교하여 ball 과 strike 카운트를 증가시키는 기능
       - printJudgeResult : 판정 결과를 콘솔에 출력하는 기능
       - continuesGame : 판정 이후 게임을 계속 진행할지에 대한 여부를 Return
    - 위 기능에 대한 테스트 코드 추가
       - 테스트 클래스 : RefereeTest
       - 테스트는 정답과 오답을 입력했을 때 게임의 진행 여부를 체크하는 continuesGame 의 boolean result 값으로 판단

---
+++ src/main/java/baseball/domain/Referee.java
@@ -0,0 +1,55 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Referee {
+
+	private int ball;
+	private int strike;
+
+	public void judgeInning(Computer computer, User user) {
+		initBallCount();
+		judgeBallCount(computer, user);
+		printJudgeResult();
+	}
+	private void initBallCount() {
+		ball = 0;
+		strike = 0;
+	}
+
+	private void judgeBallCount(Computer computer, User user) {
+		List<Integer> userNumbers = user.getNumbers();
+		List<Integer> computerNumbers= computer.getNumbers();
+		for (int userNumber : userNumbers) {
+			increaseBallCount(userNumber, userNumbers.indexOf(userNumber), computerNumbers);
+		}
+	}
+
+	private void increaseBallCount(int userNumber, int userNumberIndex, List<Integer> computerNumbers) {
+		if (computerNumbers.indexOf(userNumber) == userNumberIndex) {
+			strike++;
+			return;
+		}
+		if (computerNumbers.contains(userNumber)) {
+			ball++;
+		}
+	}
+
+	private void printJudgeResult() {
+		if (strike == 0 && ball == 0) {
+			System.out.println("낫싱");
+		}
+		StringBuilder builder = new StringBuilder();
+		if (ball != 0) {
+			builder.append(ball).append("볼").append(" ");
+		}
+		if (strike != 0) {
+			builder.append(strike).append("스트라이크");
+		}
+		System.out.println(builder.toString());
+	}
+
+	public boolean continuesGame() {
+		return strike != 3;
+	}
+}
---
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -0,0 +1,45 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RefereeTest extends AbstractDomainTest {
+
+	@Test
+	public void 게임_종료_판정_테스트() {
+		Computer computer = new Computer();
+		StringBuilder builder = new StringBuilder();
+		for (int answer : computer.getNumbers()) {
+			builder.append(String.valueOf(answer));
+		}
+		User user = setInInput(builder.toString());
+		Referee referee = new Referee();
+		referee.judgeInning(computer, user);
+		Assertions.assertFalse(referee.continuesGame());
+	}
+
+	@Test
+	public void 게임_계속_진행_테스트() {
+		Computer computer = new Computer();
+		List<Integer> answers = computer.getNumbers();
+		List<Integer> noAnswers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
+		for (int answer : answers) {
+			// index가 아닌 Object로 remove 하기위해 Integer로 박싱하여 처리
+			noAnswers.remove((Integer) answer);
+		}
+
+		StringBuilder builder = new StringBuilder();
+		for (int index = 0; index < 3; index++) {
+			int noAnswer = noAnswers.get(index);
+			builder.append(String.valueOf(noAnswer));
+		}
+		User user = setInInput(builder.toString());
+		Referee referee = new Referee();
+		referee.judgeInning(computer, user);
+		Assertions.assertTrue(referee.continuesGame());
+	}
+}
