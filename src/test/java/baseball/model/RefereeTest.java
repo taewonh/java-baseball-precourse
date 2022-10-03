902802e7b1ca90a2700316848dcc7ba8656ddf58
taewonh <40269387+taewonh@users.noreply.github.com>
2022-10-03 17:25:24 +0900

    test: 심판 테스크 코드 메서드 분리
    - 기존 정답 판정 및 오답 판정 메서드에서 정답인 유저 생성, 오답인 유저 생성, 판정 결과 메서드로 분리

--- src/test/java/baseball/model/RefereeTest.java
+++ src/test/java/baseball/model/RefereeTest.java
@@ -1,45 +1,52 @@
 package baseball.model;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class RefereeTest extends AbstractDomainTest {
 
     @Test
     public void 정답_판정_테스트() {
         Computer computer = new Computer();
-        StringBuilder builder = new StringBuilder();
-        for (int answer : computer.getNumbers()) {
-            builder.append(String.valueOf(answer));
-        }
-        User user = setInInput(builder.toString());
-        Referee referee = new Referee();
-        referee.judgeInning(computer, user);
-        Assertions.assertFalse(referee.continuesGame());
+        User user = 정답_유저_생성(computer);
+        boolean 판정_결과 = 판정_결과(computer, user);
+        Assertions.assertFalse(판정_결과);
     }
 
     @Test
     public void 오답_판정_테스트() {
         Computer computer = new Computer();
-        List<Integer> answers = computer.getNumbers();
+        User user = 오답_유저_생성(computer);
+        boolean 판정_결과 = 판정_결과(computer, user);
+        Assertions.assertTrue(판정_결과);
+    }
+
+    private User 정답_유저_생성(Computer computer) {
+        StringBuilder builder = new StringBuilder();
+        for (int answer : computer.getNumbers()) {
+            builder.append(answer);
+        }
+        return setInInput(builder.toString());
+    }
+
+    private User 오답_유저_생성(Computer computer) {
         List<Integer> noAnswers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
-        for (int answer : answers) {
-            // index가 아닌 Object로 remove 하기위해 Integer로 박싱하여 처리
+        for (int answer : computer.getNumbers()) {
             noAnswers.remove((Integer) answer);
         }
-
         StringBuilder builder = new StringBuilder();
-        for (int index = 0; index < 3; index++) {
-            int noAnswer = noAnswers.get(index);
-            builder.append(String.valueOf(noAnswer));
+        for (int count = 0; count < 3; count++) {
+            builder.append(noAnswers.get(count));
         }
-        User user = setInInput(builder.toString());
+        return setInInput(builder.toString());
+    }
+
+    private boolean 판정_결과(Computer computer, User user) {
         Referee referee = new Referee();
         referee.judgeInning(computer, user);
-        Assertions.assertTrue(referee.continuesGame());
+        return referee.continuesGame();
     }
 }
